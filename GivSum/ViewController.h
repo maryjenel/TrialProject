65041f0bd1b83c2206c3958ab33bcffe1d93df81
Mary Jenel <mmyers7@hawaii.edu>
2015-03-26 20:16:23 -0700

    Initial Commit

---
+++ GivSum/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GivSum
+//
+//  Created by Mary Jenel Myers on 3/26/27 H.
+//  Copyright (c) 27 Heisei Mary Jenel Myers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GivSum/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GivSum
+//
+//  Created by Mary Jenel Myers on 3/26/27 H.
+//  Copyright (c) 27 Heisei Mary Jenel Myers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
